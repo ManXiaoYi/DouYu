75b0bd4dd40cc4b213d4ab6af06b2dba938415ad
ManXiaoYi <994525441@qq.com>
2016-12-16 10:57:31 +0800

    设置导航栏

--- DouYu/Classes/Tools/Extension/UIBarButtonItem-Extension.swift
+++ DouYu/Classes/Tools/Extension/UIBarButtonItem-Extension.swift
@@ -9,5 +9,26 @@
 import UIKit
 
 extension UIBarButtonItem {
-    
+    // 遍历构造函数
+    // 1. convenience开头     2. 在构造函数中必须明确调用一个设计的构造函数(self)
+    convenience init(imageName: String, highImageName: String = "", size: CGSize = CGSize.zero) {
+        // 1. 创建UIButton
+        let btn = UIButton()
+        
+        // 2. 设置btn的图片
+        btn.setImage(UIImage(named: imageName), for: .normal)
+        if highImageName != "" {
+            btn.setImage(UIImage(named: highImageName), for: .highlighted)
+        }
+        
+        // 3. 设置btn的尺寸
+        if size == CGSize.zero {
+            btn.sizeToFit()
+        } else {
+            btn.frame = CGRect(origin: CGPoint.zero, size: size)
+        }
+        
+        // 4. 创建UIBarButtonItem
+        self.init(customView: btn)
+    }
 }
