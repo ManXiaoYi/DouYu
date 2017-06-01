49d077554dcc4f0415e65469a68251afbf80d00f
ManXiaoYi <994525441@qq.com>
2017-06-01 10:38:10 +0800

    全屏Pop手势

--- DouYu/Classes/Main/Controller/CustomNavigationController.swift
+++ DouYu/Classes/Main/Controller/CustomNavigationController.swift
@@ -6,12 +6,49 @@
 //  Copyright © 2017年 满孝意. All rights reserved.
 //
 
+/** 全屏pop手势
+ * 系统Pop -> View -> target/action
+ * 自定义Pan手势 -> View -> target/action
+ */
+
 import UIKit
 
 class CustomNavigationController: UINavigationController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // 1. 获取系统手势
+        guard let systemGes = interactivePopGestureRecognizer else { return }
+        
+        // 2. 获取手势添加到View中
+        guard let gesView = systemGes.view else { return }
+        
+        // 3. 获取target/action
+        // 3.1. 利用运行时机制查看所有的属性名称
+        /*
+         var count: UInt32 = 0
+         guard let ivars = class_copyIvarList(UIGestureRecognizer.self, &count) else { return }
+         for i in 0..<count {
+         let ivar = ivars[Int(i)]
+         let name = ivar_getName(ivar)
+         print(String(cString: name!))
+         }
+         */
+        let targets = systemGes.value(forKey: "_targets") as? [NSObject]
+        guard let targetObjc = targets?.first else { return }
+        print(targetObjc)
+        
+        // 3.2. 取出target
+        guard let target = targetObjc.value(forKey: "target") else { return }
+        
+        // 3.3. 取出action
+        let action = Selector(("handleNavigationTransition:"))
+        
+        // 4. 创建自己的Pan手势
+        let panGes = UIPanGestureRecognizer()
+        gesView.addGestureRecognizer(panGes)
+        panGes.addTarget(target, action: action)
     }
     
     override func pushViewController(_ viewController: UIViewController, animated: Bool) {
